97aa04188c4368eb72d5ca489472bc6dfa220df5
Jolgo <jlgo60@hotmail.com>
2020-10-31 17:00:05 -0500

    Se agrego aler dialogo

--- app/src/main/java/com/example/vivasantamarta/MainActivity.kt
+++ app/src/main/java/com/example/vivasantamarta/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.vivasantamarta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+    }
+
+    override fun onBackPressed() {
+        MaterialAlertDialogBuilder(this)
+            .setMessage("¿Desea salir de la aplicación?")
+
+            .setNegativeButton("No") { dialog, which ->
+                // Respond to negative button press
+            }
+            .setPositiveButton("Si") { dialog, which ->
+                // Respond to positive button press
+
+                finish()
+            }
+            .show()
+
     }
 }
